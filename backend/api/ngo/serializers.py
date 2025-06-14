910933a24582d659b2d2c4e2ab4b8892867a9a23
nishu-02 <0264nishu@gmail.com>
2025-06-14 22:05:35 +0530

    NGO register View

---
+++ backend/api/ngo/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import NGO
+
+class NGORegisterSerializer(serializers.ModelSerializer)
+    class Meta:
+        model = NGO
+        fields = [
+            'ngo_id', 'name', 'email', 'phone',
+            'latitude', 'longitude', 'category',
+            'description', 'website'
+        ]
+        extra_kwargs = {
+            'ngo_id':{
+                'read_only':True
+            },
+        }
--- backend/api/ngo/views.py
+++ backend/api/ngo/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import NGO
+from .serializers import NGORegisterSerializer
 
-# Create your views here.
+import appwrite
+from appwrite.client import Client
+from appwrite.serivces.account import Account
+
+from django.conf import settings
+
+class RegisterNGOView(APIView):
+    def post(self, request):
+        token = request.headers.get("X-Appwrite-Token")
+        if not token or not starts with X:
+            return Response({
+                "error": "Token Missing"
+            }, status=status.HTTP_401_UNAUTHORIZED  )
+
+        # Connecting to Appwrite
+        client = Client()
+        client.set_endpoint(settings.APPWRITE_ENDPOINT)
+        client.set_project(settings.APPWRITE_PROJECT_ID)
+        client.set_key(settings.APPWRITE_API_KEY)
+        account = Account(client)
+
+        try:
+            session = account.get_session('current', {
+                "X-Appwrite-Session": token
+            })
+            user_id = session['userId']
+        except Exception as e:
+            return Response({
+                "error": "Invalid Appwrite Token"
+            }, status=status.HTTP_401_UNAUTHORIZED)
+
+        # Saving the NGO info
+        data = request.data.copy()
+        data['ngo_id'] = user_id
+
+        serializer = NGORegisterSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "message": "Your NGO registers successfully"
+            }, status=status.HTTP_201_CREATED)
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
