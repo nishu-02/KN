f667f92030a01b3116f46bcb285a3150b691f9d0
nishu-02 <0264nishu@gmail.com>
2025-06-25 00:03:10 +0530

    Notification part 3

--- backend/api/notifications/models.py
+++ backend/api/notifications/models.py
@@ -1,14 +1,26 @@
 import uuid
 from django.db import models
+from reports.models import InjuryReport
 
 class NotificationHistory(models.Model):
-    id = models.UUIDField(primary=True, default=uuid.uuid4, editable=False)
-    user_id = models.CharField(max_length=255) # can be normal user or the NGO
-    title = models.CharField(max_length)
+    notification_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    report = models.ForeignKey(InjuryReport, on_delete=models.CASCADE, related_name="notifications")
+
+    recipient_id = models.CharField(max_length=255)  # can be normal user or the NGO
+    recipient_type = models.CharField(max_length=10,
+        choices=[
+            ("ngo", "NGO"),
+            ("user", "User"),
+        ]
+    )
+
+    title = models.CharField(max_length=255)
     body = models.TextField()
     data = models.JSONField(blank=True, null=True)
+
+    status = models.CharField(max_length=20, default='sent')
     is_read = models.BooleanField(default=False)
-    created_at = models.DataTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-created-at']
+        ordering = ['-created_at']
--- backend/api/notifications/views.py
+++ backend/api/notifications/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.reponse import Response
+from rest_framework import status
+
+from .models import NotificationHistory
+
 from reports.permissions import isAppwriteUser
 
-class UserNotificationList(APIView):
+
+class UserNotificationListView(APIView):
     permission_classes = [isAppwriteUser]
 
     def get(self, request):
@@ -19,3 +24,29 @@ class UserNotificationList(APIView):
         return Response({
             "notifications": data
         })
+
+class MarkNotificationAsViewed(APIView):
+    permission_classes = [isAppwriteUser]
+
+    def patch(self, request, notification_id):
+        try:
+            notification = NotificationHistory.objects.get(notification_id=notification_id)
+
+            if notification.ngo_id != request.user_id:
+                return Response({
+                    "error": "Unauthorized"
+                },status=status.HTTP_401_UNAUTHORIZED)
+
+            notification.is_viewed = True
+            notification.save()
+
+            serializer = NotificationHistorySerializer(notification)
+            return Response (
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+
+        except NotificationHistory.DoesNotExist:
+            return Response({
+                "error": "Notification not found"
+            }, status=status.HTTP_404_NOT_FOUND)
